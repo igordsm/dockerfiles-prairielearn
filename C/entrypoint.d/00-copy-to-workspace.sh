b041a273e985a61ea5a79f5d9b7f2b9add2712f3
Igor Montagner <igorsm1@insper.edu.br>
2025-09-01 08:10:48 -0300

    Missing files.

---
+++ C/entrypoint.d/00-copy-to-workspace.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+echo "Copying files to workspace folder"
+
+cp /home/coder/template/Makefile /home/coder/project
+mkdir -p /home/coder/project/.vscode/
+cp /home/coder/template/tasks.json /home/coder/project/.vscode/
+cp /home/coder/template/settings.json /home/coder/project/.vscode/

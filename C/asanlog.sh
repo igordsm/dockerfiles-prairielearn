e3776db7b3a591903bdb7018fa4c58d5584f3c66
Dev Singh <dev@devksingh.com>
2025-09-20 12:53:38 -0500

    Zip up the logs for submission to PL

--- C/asanlog.sh
+++ C/asanlog.sh
@@ -13,3 +13,5 @@ RUN_ID=$(date +"%Y%m%d_%H%M%S")
 export LOG_FILE_PREFIX="${LOG_DIR}/report_${RUN_ID}.log" 
 echo "Run ID: $RUN_ID"
 ASAN_OPTIONS="log_path=${LOG_FILE_PREFIX}:continue_on_error=1" ASANLOG_ENABLED="true" "$@"
+rm -f /home/coder/project/asanlogs.zip
+zip -r --junk-paths /home/coder/project/asanlogs.zip /home/coder/.asan_logs > /dev/null 2>&1 || true

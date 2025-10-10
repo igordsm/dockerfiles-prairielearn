abfd2962bc94ae87afac9c5d87f64094ec542a90
Dev Singh <dev@devksingh.com>
2025-10-10 14:42:43 -0500

    Collect stdout for all tests ran

--- C/asanlog.sh
+++ C/asanlog.sh
@@ -10,8 +10,9 @@ LOG_DIR="/home/coder/.asan_logs"
 mkdir -p "$LOG_DIR"
 
 RUN_ID=$(date +"%Y%m%d_%H%M%S")
-export LOG_FILE_PREFIX="${LOG_DIR}/report_${RUN_ID}.log" 
+export LOG_FILE_PREFIX="${LOG_DIR}/report_${RUN_ID}.log"
+export TEST_OUTPUT="${LOG_DIR}/tests_${RUN_ID}.log"
 echo "Run ID: $RUN_ID"
-ASAN_OPTIONS="log_path=${LOG_FILE_PREFIX}:continue_on_error=1" ASANLOG_ENABLED="true" "$@"
+ASAN_OPTIONS="log_path=${LOG_FILE_PREFIX}:continue_on_error=1" ASANLOG_ENABLED="true" "$@" | tee -a "$TEST_OUTPUT"
 rm -f /home/coder/project/asanlogs.zip
 zip -r --junk-paths /home/coder/project/asanlogs.zip /home/coder/.asan_logs > /dev/null 2>&1 || true
--- C/envcheck.c
+++ C/envcheck.c
@@ -7,11 +7,11 @@ void run_env_check(void) __attribute__((constructor));
 const char* message = "You must run this executable with the `asanlog` binary. For example: `asanlog ./test`.\n";
 void run_env_check(void) {
     if (getenv("ASANLOG_ENABLED") == NULL || strcmp(getenv("ASANLOG_ENABLED"), "true") != 0) {
-        fprintf(stderr, message);
+        fprintf(stderr, "%s", message);
         exit(125);
     }
     if (getenv("ASAN_OPTIONS") == NULL) {
-        fprintf(stderr, message);
+        fprintf(stderr, "%s", message);
         exit(126);
     }
 }
